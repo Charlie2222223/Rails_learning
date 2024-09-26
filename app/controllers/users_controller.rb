307c1be20d6b2a3b61520c427633ca27cd665cc4
kaito furuya <kaito.furuya0806@gmail.com>
2024-09-26 15:07:31 +0900

    ユーザーと部署とスキルののCRUD機能を実装しました

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,49 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private user_params
+    def
+      params.require(:user).permit(:department_id, :name, :ruby, :sex, :tel, 
+                                  :mobile, :mail,:zip, :address1, :address2, 
+                                  :address3, :address4, :address5,:birthday)
+    end
+end
