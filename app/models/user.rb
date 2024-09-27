e861a1961b053184cc84f7c7888952d064262e87
TakuyaArai6I20VV <leetgrow@gmail.com>
2024-09-27 11:20:43 +0900

    user modelの単体テストを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  belongs_to :department
+
+  validates :name, presence: true
+  validates :mail, format: { with: URI::MailTo::EMAIL_REGEXP, message: "invalid email format" }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @department = Department.create(name: 'Test Department')
+    @user = User.new(
+      department_id: @department.id,
+      name: 'Test User',
+      ruby: 'てすとゆーざー',
+      sex: 'male',
+      tel: '1234567890',
+      mobile: '0987654321',
+      mail: 'testuser@example.com',
+      zip: '123-4567',
+      address1: 'Test Address 1',s
+      address2: 'Test Address 2',
+      birthday: '1990-01-01'
+    )
+  end
+
+  test "should save valid user" do
+    assert @user.save
+  end
+
+  test "should not save user without name" do
+    @user.name = nil
+    assert_not @user.save
+  end
+
+  test "should not save user with invalid email" do
+    @user.mail = 'invalid_email'
+    assert_not @user.save
+  end
 end
